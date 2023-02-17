755384d2e1336c58e038448f09b0ace181dec31d
Asier Arronategui Basurto <65606861+Kustonm@users.noreply.github.com>
2023-02-17 14:31:24 +0100

    Small Fixes

--- PL1a/search.py
+++ PL1a/search.py
@@ -64,19 +64,6 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    """
     "*** YOUR CODE HERE ***"
     
     movs = util.Stack()
@@ -116,13 +103,12 @@ def breadthFirstSearch(problem):
             for s in x: movs.push((s[0],state[1]+','+s[1]))
 
 def uniformCostSearch(problem):
-    movs = util.Queue()
+    movs = util.PriorityQueue()
     been = []
     acts =[]
     first=(problem.getStartState(),'',0)
-    movs.push(first)
+    movs.push(first,first[2])
     while not  movs.isEmpty():
-        movs.list.sort(key=lambda x: x[2],reverse=True)
         state = movs.pop()
         if problem.isGoalState(state[0]):
             acts = state[1].split(',')
@@ -131,9 +117,8 @@ def uniformCostSearch(problem):
         if state[0] not in been:
             been.append(state[0])
             x = problem.getSuccessors(state[0])
-            sorted(x,key=lambda x: x[2],reverse=True)
             for s in x: 
-                if s[0] not in been: movs.push((s[0],state[1]+','+s[1],state[2]+s[2]))
+                if s[0] not in been: movs.update((s[0],state[1]+','+s[1],state[2]+s[2]),state[2]+s[2])
                     
 
 def nullHeuristic(state, problem=None):
